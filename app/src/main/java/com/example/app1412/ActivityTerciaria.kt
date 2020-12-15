0dee1389216cda4ade46c4d4063ff3df4a4ae79e
T35000255 <jose.fernandes@compasso.com.br>
2020-12-15 20:02:11 -0300

    Terceiro commit

---
+++ app/src/main/java/com/example/app1412/ActivityTerciaria.kt
@@ -0,0 +1,11 @@
+package com.example.app1412
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTerciaria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terciaria)
+    }
+}
